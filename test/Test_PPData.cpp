e7828f07c0ff67fc33356ee4f49254d764aba711
Jiaan DAI <jdaiab@connect.ust.hk>
2016-09-10 15:33:26 +0800

    Update tests

--- test/Test_PPData.cpp
+++ test/Test_PPData.cpp
@@ -1,22 +1,28 @@
-#include "PeptMgr.h"
+#include "PPData.h"
+#include <ProtData.h>
 #include <gtest/gtest.h>
 
-TEST(Unittest_PeptMgr, TrypsinDigest) {
-    auto peptset1 = PeptMgr("uniprot-all.fasta", false, PeptGenerator::EnzymeType::Trypsin, 0);
-    auto peptides = peptset1.Retrieve();
-    EXPECT_EQ(554376, peptides.size());
-    auto iters = peptset1.RetrieveMassRange(700, 5000);
-    EXPECT_EQ(554376, std::distance(iters.first, iters.second));
+TEST(Unittest_PPData, PPData_API) {
+//    PPData ppdata("uniprot-all.fasta", false, PPData::EnzymeType::Trypsin, 0, 700, 5000);
+//    EXPECT_EQ(554376, ppdata.size());
+//    auto range = ppdata.LoopWithin(700, 5000);
+//    EXPECT_EQ(554376, std::distance(range.begin(), range.end()));
 
-    auto peptset2 = PeptMgr("uniprot-all.fasta", false, PeptGenerator::EnzymeType::Trypsin, 1);
-    peptides = peptset2.Retrieve();
-    EXPECT_EQ(1465360, peptides.size());
-    iters = peptset2.RetrieveMassRange(700, 5000);
-    EXPECT_EQ(1465360, std::distance(iters.first, iters.second));
+//    PPData ppdata2("uniprot-all.fasta", false, PPData::EnzymeType::Trypsin, 1, 700, 5000);
+//    EXPECT_EQ(1465360, ppdata2.size());
+//    iters = peptset2.RetrieveMassRange(700, 5000);
+//    EXPECT_EQ(1465360, std::distance(iters.first, iters.second));
 
-    auto peptset3 = PeptMgr("uniprot-all.fasta", false, PeptGenerator::EnzymeType::Trypsin, 2);
-    peptides = peptset3.Retrieve();
-    EXPECT_EQ(2404590, peptides.size());
-    iters = peptset3.RetrieveMassRange(700, 5000);
-    EXPECT_EQ(2404590, std::distance(iters.first, iters.second));
+    PPData ppdata3("uniprot-all.fasta", false, PPData::EnzymeType::Trypsin, 2, 700, 5000);
+    EXPECT_EQ(2404590, ppdata3.size());
+//    iters = peptset3.RetrieveMassRange(700, 5000);
+//    EXPECT_EQ(2404590, std::distance(iters.first, iters.second));
+}
+
+TEST(Unittest_PPData, ProtData_API) {
+    auto fasta = ProtData("uniprot-all.fasta", false);
+    EXPECT_EQ(20198, fasta.size());
+
+    auto decoy = ProtData("uniprot-all.fasta", true);
+    EXPECT_EQ(20198 * 2, decoy.size());
 }
